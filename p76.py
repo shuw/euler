18c8ad51df8aa3c3ad49da77509469b066d0f57b
Shu Wu <shuwu83@gmail.com>
2012-09-11 23:53:21 -0700

    no need to subtract 1 if we skip component == N

--- p76.py
+++ p76.py
@@ -2,11 +2,10 @@ def solve(N):
     """Dynamic Programming approach solution to project Euler 76"""
     # initial 1 indicates that a number can always be a sum of itself
     ways = [1] + [0] * N
-    for component in range(1, N + 1):
+    for component in range(1, N):
         for index in range(N + 1 - component):
             ways[index + component] += ways[index]
 
-    # subtract 1 because we only want sums of 2 integers
-    return ways[-1] - 1
+    return ways[-1]
 
 print(solve(100))
