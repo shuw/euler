aa6fc37bd950e4e5757e5f33d77781284fd16eed
Shu Wu <shuwu83@gmail.com>
2012-09-20 22:45:40 -0700

    use bablynon method

--- python/practice/sqrt.py
+++ python/practice/sqrt.py
@@ -2,36 +2,33 @@ import math
 
 
 def sqrt(n):
-    lower_estimate = 0
-    upper_estimate = n
-    margin = n * 0.00000001
-
-    estimate = upper_estimate / 2
+    estimate = n / 2.0
     iterations = 0
 
     while True:
         iterations += 1
-        error = (estimate * estimate) - n
-        if math.fabs(error) < margin:
+        bound = n / estimate
+        if math.fabs((bound - estimate) / estimate) <= 0.000000001:
             return (estimate, iterations)
 
-        # too big
-        if error > 0:
-            upper_estimate = estimate
-        else:
-            lower_estimate = estimate
-
-        estimate = (upper_estimate + lower_estimate) / 2.0
+        estimate = (estimate + bound) / 2.0
 
 
 def test(n):
     expected = math.sqrt(n)
     actual, iterations = sqrt(n)
     error = math.fabs(expected - actual)
-    accuracy = (expected - error) / expected
 
-    print("expected: %s, actual: %s, accuracy: %s, iterations: %s" % \
-        (expected, actual, accuracy, iterations))
+    if expected:
+        accuracy = (expected - error) / expected
+    else:
+        accuracy = 'n/a'
+
+    print("accuracy: %s, expected: %s, actual: %s, iterations: %s" % \
+        (accuracy, expected, actual, iterations))
 
 if __name__ == '__main__':
+    test(1)
+    test(4)
     test(61234234423455)
+    test(232379238546328645235)
